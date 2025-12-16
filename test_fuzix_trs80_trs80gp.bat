f53f472dfeadd5c78bc5c22b86632b7748f68bab
Michel Bernard <michel_bernard@icloud.com>
2025-12-16 10:28:19 +0100

    cosmetics

--- test_fuzix_trs80_trs80gp.bat
+++ test_fuzix_trs80_trs80gp.bat
@@ -6,6 +6,6 @@ gettrs80gp.sh
 if errorlevel 1 pause && goto :eof
 :TRS80GP_OK
 
-echo :: Starting the emulator
+echo :: Starting the emulator ::
 set FUZIX=mount\FUZIX\Images\trs80
 windows\trs80gp.exe -m4ga -gt -d %FUZIX%\boot.jv3 -h0 %FUZIX%\hard4p-0 -vs -win full
